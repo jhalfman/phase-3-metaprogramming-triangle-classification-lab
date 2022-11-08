5dfe4b1672f7155409f87837c6d6fe423c35f3a7
Jordan Halfman <jordanhalfman@gmail.com>
2022-11-08 11:27:16 -0700

    commit tree ee8bba54e3197421ecd340943db83e9f8adedf5f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if (a <= 0 || b <= 0 || c <= 0)
+      
+        raise TriangleError
+      
+        
+    
+    elsif (a + b <= c || a + c <= b || b + c <= a)
+      
+        raise TriangleError
+      
+       
+      
+    elsif (a == b && b == c)
+      :equilateral
+    elsif (a != b && b != c && c!= a)
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+
+    def nonpositive_side_length
+      "All side lengths must be positive values"
+    end
+
+    def triangle_inequality
+      "This triangle breaks the triangle inequality rule"
+    end    
+
+  end
 end
+
